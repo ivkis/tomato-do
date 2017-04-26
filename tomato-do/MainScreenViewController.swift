6d92910c4b99f8a24b1f75dafbd9df48c739f640
ivkis <ivanlazarevkis@gmail.com>
2017-04-26 18:31:42 +0300

    Created the main screen.

---
+++ tomato-do/MainScreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainScreenViewController.swift
+//  tomato-do
+//
+//  Created by Иван Лазарев on 25/04/2017.
+//  Copyright © 2017 Иван Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    @IBOutlet weak var addTaskButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMainScreenViewController()
+    }
+    
+    func setupMainScreenViewController() {
+    }
+}
+
