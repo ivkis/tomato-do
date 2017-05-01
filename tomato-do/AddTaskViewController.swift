44b149c8f589969f1f95062a7d81f39583281dfe
ivkis <ivanlazarevkis@gmail.com>
2017-05-01 18:10:56 +0300

    Added by R.Swift and SwiftLint in Podfile

---
+++ tomato-do/AddTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddTaskViewController.swift
+//  tomato-do
+//
+//  Created by Иван Лазарев on 01/05/2017.
+//  Copyright © 2017 Иван Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AddTaskViewController()
+    }
+    
+    func AddTaskViewController() {
+    }
+    
+}
+
+
