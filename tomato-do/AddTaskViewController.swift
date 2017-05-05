aebc45bca3cf13cdf8dd38409e8a2f4e892d139f
ivkis <ivanlazarevkis@gmail.com>
2017-05-05 16:42:56 +0300

    Created screen adding tasks

--- tomato-do/AddTaskViewController.swift
+++ tomato-do/AddTaskViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class AddTaskViewController: UIViewController {
+class AddTaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
+    @IBOutlet weak var addTaskTextField: UITextField!
+    @IBOutlet weak var addTaskTableView: UITableView!
+    
+    var tasks: [Task] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAddTaskViewController()
+        addTaskTextField.delegate = self
     }
     
     func setupAddTaskViewController() {
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.taskCell)
+        cell?.textLabel?.text = tasks[indexPath.row].title
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let newTask: Task = Task(title: addTaskTextField.text!)
+        tasks.append(newTask)
+        addTaskTextField.text = ""
+        addTaskTableView.reloadData()
+        self.view.endEditing(true)
+        return true
+    }
 }
 
 
