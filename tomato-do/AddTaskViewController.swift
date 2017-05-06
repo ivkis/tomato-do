75afd7312ff3b9a4275ea769e194d64bf3cc5958
ivkis <ivanlazarevkis@gmail.com>
2017-05-06 19:25:36 +0300

    Save data using CoreData

--- tomato-do/AddTaskViewController.swift
+++ tomato-do/AddTaskViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
@@ -14,7 +15,20 @@ class AddTaskViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var addTaskTableView: UITableView!
     
     var tasks: [Task] = []
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetcRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            tasks = try context.fetch(fetcRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAddTaskViewController()
@@ -26,7 +40,8 @@ class AddTaskViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.taskCell)
-        cell?.textLabel?.text = tasks[indexPath.row].title
+        let task = tasks[indexPath.row]
+        cell?.textLabel?.text = task.taskToDo
         return cell!
     }
     
@@ -35,12 +50,27 @@ class AddTaskViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        let newTask: Task = Task(title: addTaskTextField.text!)
-        tasks.append(newTask)
+        self.saveTask(taskToDo: (textField.text)!)
         addTaskTextField.text = ""
         addTaskTableView.reloadData()
         self.view.endEditing(true)
         return true
+    }
+    
+    func saveTask(taskToDo: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
+        let taskObject = NSManagedObject(entity: entity!, insertInto: context) as! Task
+        taskObject.taskToDo = taskToDo
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
