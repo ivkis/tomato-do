b1ea25441c5c520e6117a6d920d08b43b482045f
ivkis <ivanlazarevkis@gmail.com>
2017-05-17 12:58:32 +0300

    Logic work with CoreData moved into a separate class

--- tomato-do/AddTaskViewController.swift
+++ tomato-do/AddTaskViewController.swift
@@ -14,24 +14,15 @@ class AddTaskViewController: UIViewController, FMMoveTableViewDelegate, FMMoveTa
     
     @IBOutlet weak var addTaskTextField: UITextField!
     @IBOutlet weak var addTaskTableView: FMMoveTableView!
+
     @IBAction func pomodoroGoTimer(_ sender: Any) {
         let conroller = R.storyboard.main.pomodoroViewController()!
         navigationController?.pushViewController(conroller, animated: true)
     }
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    var tasks: [Task] = []
-    
     override func viewWillAppear(_ animated: Bool) {
-        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "positionTask", ascending: true)]
-        
-        do {
-            tasks = try context.fetch(fetchRequest)
-        } catch {
-            print(error.localizedDescription)
-        }
+        super .viewWillAppear(animated)
+        CoreDataManager.shared.downloadFromCoreData()
     }
     
     override func viewDidLoad() {
@@ -40,33 +31,23 @@ class AddTaskViewController: UIViewController, FMMoveTableViewDelegate, FMMoveTa
         addTaskTextField.delegate = self
     }
     
-    func moveTableView(_ tableView: FMMoveTableView!, moveRowFrom fromIndexPath: IndexPath!, to toIndexPath: IndexPath!) {
-        guard fromIndexPath.row != toIndexPath.row else {
-            return
-        }
-        tasks.rearrange(from: fromIndexPath.row, to: toIndexPath.row)
-        saveArrayInCoreData()
-        addTaskTableView.reloadData()
-    }
-
     func setupAddTaskViewController() {
         navigationController?.navigationBar.isHidden = true
     }
     
-    func saveArrayInCoreData() {
-        for (index, task) in tasks.enumerated() {
-            task.positionTask = Int16(index)
+    func moveTableView(_ tableView: FMMoveTableView!, moveRowFrom fromIndexPath: IndexPath!, to toIndexPath: IndexPath!) {
+        guard fromIndexPath.row != toIndexPath.row else {
+
+            return
         }
-        do {
-            try context.save()
-        } catch {
-            print(error.localizedDescription)
-        }
+
+        CoreDataManager.shared.rearrange(from: fromIndexPath.row, to: toIndexPath.row)
+        addTaskTableView.reloadData()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.taskCell)
-        let task = tasks[indexPath.row]
+        let task = CoreDataManager.shared.tasks[indexPath.row]
         cell?.textLabel?.font = UIFont(name:"Courier", size:18)
         cell?.textLabel?.text = task.taskToDo
 
@@ -76,30 +57,22 @@ class AddTaskViewController: UIViewController, FMMoveTableViewDelegate, FMMoveTa
 
         return cell!
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return tasks.count
+        return CoreDataManager.shared.tasks.count
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.saveTask(taskToDo: (textField.text)!)
+        CoreDataManager.shared.addTask(taskToDo: (textField.text)!)
         addTaskTextField.text = ""
         addTaskTableView.reloadData()
         self.view.endEditing(true)
 
         return true
-    }
-    
-    func saveTask(taskToDo: String) {
-        let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
-        let taskObject = NSManagedObject(entity: entity!, insertInto: context) as! Task
-        taskObject.taskToDo = taskToDo
-        tasks.insert(taskObject, at: 0)
-        saveArrayInCoreData()
     }
 
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -108,15 +81,7 @@ class AddTaskViewController: UIViewController, FMMoveTableViewDelegate, FMMoveTa
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        let taskToDelete = tasks[indexPath.row]
-        context.delete(taskToDelete)
-
-        do {
-            try context.save()
-            tasks.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        } catch let error as NSError {
-            print("Error: \(error), description \(error.userInfo)")
-        }
+        CoreDataManager.shared.deleteTask(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
     }
 }
---
+++ tomato-do/CoreDataManager.swift
@@ -0,0 +1,66 @@
+//
+//  CoreDataManager.swift
+//  tomato-do
+//
+//  Created by IvanLazarev on 17/05/2017.
+//  Copyright © 2017 Иван Лазарев. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataManager {
+
+    static let shared = CoreDataManager()
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private(set) var tasks = [Task]()
+
+    func addTask(taskToDo: String) {
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
+        let taskObject = NSManagedObject(entity: entity!, insertInto: context) as! Task
+        taskObject.taskToDo = taskToDo
+        tasks.insert(taskObject, at: 0)
+        saveArrayInCoreData()
+    }
+
+    func deleteTask(at index: Int) {
+        let taskToDelete = tasks[index]
+        context.delete(taskToDelete)
+
+        do {
+            try context.save()
+            tasks.remove(at: index)
+        } catch let error as NSError {
+            print("Error: \(error), description \(error.userInfo)")
+        }
+    }
+
+    func saveArrayInCoreData() {
+        for (index, task) in tasks.enumerated() {
+            task.positionTask = Int16(index)
+        }
+
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func downloadFromCoreData() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "positionTask", ascending: true)]
+
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func rearrange(from: Int, to: Int) {
+        rearrange(from: from, to: to)
+        saveArrayInCoreData()
+    }
+
+}
