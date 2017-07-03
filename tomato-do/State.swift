34aa431b14b3ad6ca3ea6fb155e37a3e5f072b35
ivkis <ivanlazarevkis@gmail.com>
2017-07-03 14:28:48 +0300

    Added check date of last run

--- tomato-do/State.swift
+++ tomato-do/State.swift
@@ -12,7 +12,9 @@ import UIKit
 class State {
 
     static let shared = State()
+
     let defaults = UserDefaults.standard
+    let currentDate = Date()
 
     var counterTimer: Int {
         didSet {
@@ -25,6 +27,12 @@ class State {
     }
 
     init() {
-        self.counterTimer = max(1, defaults.integer(forKey: "counterTimer"))
+        let lastRunDate = defaults.object(forKey: "dateLastRun") as? Date
+        if let lastRunDate = lastRunDate, Calendar.current.compare(lastRunDate, to: currentDate, toGranularity: .day) == .orderedSame {
+            self.counterTimer = max(1, defaults.integer(forKey: "counterTimer"))
+        } else {
+            self.counterTimer = 1
+        }
+        defaults.set(currentDate, forKey: "dateLastRun")
     }
 }
